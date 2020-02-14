ac2f7e6620488747b2dc4482b80d1977c91eda3e
Jeremy Bedolla <jjbedolla@gmail.com>
2020-02-14 18:05:46 -0500

    adding updated __intit__

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from models.engine.file_storage.py import FileStorage
+from models.engine.file_storage import FileStorage
 
-storage = FileStorage
+storage = FileStorage()
 storage.reload()
