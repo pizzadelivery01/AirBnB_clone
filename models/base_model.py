c764a5f31d5e4828cb47815d5e7f9d6d5ba5f2a7
Gzoref <gzoref@gmail.com>
2020-02-14 13:15:34 -0500

    Add base_model v1.2

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
 
 '''
-Write a class BaseModel that defines all common attributes/methods for other classes
+Write a class BaseModel that defines all common attributes/methods
+for other classes
 '''
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     '''
     BaseModel class
     '''
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         '''
         Constructor method
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            self.__dict__.update(kwargs)
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
 
     def save(self):
         '''
         Update public instance with current datetime
         '''
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''
+        String reprsentation
+        '''
+        return '[{}] ({}) {}'.format(
+            type(self).__name__, self.id, self.__dict__)
 
     def to_dict(self):
         '''
         Dictionary containing all
         keys/values of __dict__
         '''
-        my_dict = {}
-        my_dict.update(self.__dict__)
-        self.__dict__['created at'] = self.created_at.isoformat()
-        self.__dict__['updated_at'] = self.updated_at.isoformat()
+        my_dict = dict(self.__dict__)
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['__class__'] = type(self).__name__
         return my_dict
-
-
-    def __str__(self):
-        return '[{}] ({}) {}'.format(
-            BaseModel.__class__.__name__, self.id, self.__dict__)
