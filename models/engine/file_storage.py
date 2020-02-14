7b1f1fc8f2c7df02a14374eec85457f9579714e5
Gzoref <gzoref@gmail.com>
2020-02-14 13:47:22 -0500

    Add file_storage v1.0

---
+++ models/engine/file_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Serialized instances to a JSON file and deserializes
+in JSON file to instances
+'''
+
+class FileStorage:
+    '''
+    FileStorage class
+    '''
+    def __init__(self):
+        '''
+        Class COnstructor
+        '''
+
+    def all(self):
+        '''
+        Returns the dictiornary
+        '''
+    def new(self, obj):
+        '''
+        Sets in __objects the obj with key
+        <obj class name>.id
+        '''
+    def save(self):
+        '''
+        Deserializes the JSON file
+        '''
