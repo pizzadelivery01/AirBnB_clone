5e7153287eef43fff3f419931ab33befba267fcd
Jeremy Bedolla <jjbedolla@gmail.com>
2020-02-17 14:32:17 -0500

    debugging changes

--- console.py
+++ console.py
@@ -5,9 +5,20 @@ Contains entry point of command interpretter
 import cmd
 from models.base_model import BaseModel
 from models.__init__ import storage
-from models.users import Users
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
-Class_Dict = {"BaseModel": BaseModel, "User": User, "Place": Place, "City": City, "State":, State, "Amenity": Amenity, "Review": Review}
+Class_Dict = {"BaseModel": BaseModel,
+              "User": User,
+              "Place": Place,
+              "State": State,
+              "Amenity": Amenity,
+              "Review" : Review,
+              "City" : City}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -55,9 +66,9 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print('** class name missing **')
             return
-        elif arg in Class_Dict:
+        elif args in Class_Dict:
             for key, value in Class_Dict.items():
-                if key == arg:
+                if key == args:
                     new_instance = Class_Dict[key]()
             storage.save()
             print(new_instance.id)
@@ -79,13 +90,13 @@ class HBNBCommand(cmd.Cmd):
         class_name = new_instance[0]
         class_id = new_instance[2]
         
-        if not arg:
+        if not args:
             print('** class name missing **')
         elif class_name not in Class_Dict:
             print("** Class doesn't exitst **")
         elif len(new_instance) >= 1:
             try:
-                new_objects = FileStorage.all(self)
+                new_objects = storage.all()
                 new_key = class_name + '.' + class_id
                 flag = 0
                 for key, values in new_objects.items():
@@ -94,8 +105,8 @@ class HBNBCommand(cmd.Cmd):
                         print(values)
                 if flag == 0:
                     print("** no instance found **")
-            except indexError:
-            print('** instance id missing **')
+            except IndexError:
+                print('** instance id missing **')
 
     def help_show(self):
         '''
@@ -103,26 +114,27 @@ class HBNBCommand(cmd.Cmd):
         '''
         print('Show command to show string representation\n')
 
-    def do_destroy(self, args):
+    def do_destroy(self, arg):
         '''
         Deletes an instance basesd on
         class name and id
         '''
-        new_instance = args.partition(' ')
-        class_name = new_instance[0]
-        class_id = new_instance[2]
-        if not class_name:
+        new_args = arg.partition(" ")
+        if not arg:
             print('** class name missing **')
-            return
-        if not class_id:
-            print('** instance id missing **')
-            return
-
-        key = class_name + '.' + class_id
-        try:
-            del (storage._FileStorage__objects[key])
-        except KeyError:
-            print('** no instance found **')
+        elif new_args[0] not in Class_Dict:
+            print("** class doesn't exist **")
+        elif len(new_args) <= 1:
+            try:
+                new_objects = storage.all()
+                new_key = new_args[0] + '.' + new_args[1]
+                try:
+                    new_objects.pop(new_key)
+                    storage.save()
+                except KeyError:
+                    print("** no instance found **")
+            except IndexError:
+                    print("** instance id missing **")
 
     def help_destroy(self):
         '''
@@ -135,7 +147,7 @@ class HBNBCommand(cmd.Cmd):
         new_arg = arg.partition(" ")
         if not arg:
             new_list = []
-            new_objects = FileStorage.all(self)
+            new_objects = storage.all()
             for key, values in new_objects.items():
                 new_list.append(str(values))
             print(new_list)
@@ -143,9 +155,9 @@ class HBNBCommand(cmd.Cmd):
             print("** Class doesn't exitst **")
         else:
             new_list = []
-            new_objects = FileStorage.all(self)
+            new_objects = storage.all()
             for key, values in new_objects.items():
-                new_key = key.partiion(".")
+                new_key = key.split(".")
                 if new_key[0] == new_arg[0]:
                     new_list.append(str(values))
                 print(new_list)
