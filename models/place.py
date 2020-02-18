c5e6b3cf89e65d401a47e859cbde156c5a2fcd57
Jeremy Bedolla <jjbedolla@gmail.com>
2020-02-18 14:05:18 -0500

    added inheritence

--- models/place.py
+++ models/place.py
@@ -19,3 +19,9 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+    
+def __init__(self, *args, **kwargs):
+    """
+    init
+    """
+    super().__init__(*args, **kwargs)
