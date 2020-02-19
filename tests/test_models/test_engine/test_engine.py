3aa9b01ae0d57b5166eb533f34575cd43360d363
Gzoref <gzoref@gmail.com>
2020-02-19 15:55:04 -0500

    Fix test

--- tests/test_models/test_engine/test_engine.py
+++ tests/test_models/test_engine/test_engine.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import unittest
+from models import storage
 from models.base_model import BaseModel
 import os
 
@@ -13,7 +14,7 @@ class TestFileStorage(unittest.TestCase):
         '''
         File creation test
         '''
-        new = BaseModel()
+        storage.save()
         self.assertTrue(os.path.exists('JSONstorage.json'))
 
     def tearDown(self):
@@ -33,4 +34,4 @@ class TestFileStorage(unittest.TestCase):
         my_dict = base.to_dict()
         base.save()
         base2 = BaseModel(**my_dict)
-        self.assertNotEqual(os.path.getsize('JSONstorage.json'), 5008)
+        self.assertFalse(os.stat('JSONstorage.json').st_size == 0)
